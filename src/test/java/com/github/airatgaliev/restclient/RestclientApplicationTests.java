af3c06469fab100a6e59b5a47da1bb5dc57bc1ed
Airat Galiev <airat.galiev@gmail.com>
2021-01-02 12:26:22 +0300

    created new project

---
+++ src/test/java/com/github/airatgaliev/restclient/RestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
